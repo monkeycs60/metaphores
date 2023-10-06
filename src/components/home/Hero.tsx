b692fa5035aaa33c19c3c5fc18b6c3d2b4c07923
monkeycs60 <clement.serizay@gmail.com>
2023-10-06 11:55:31 +0200

    style: setting up hero

--- src/components/home/Hero.tsx
+++ src/components/home/Hero.tsx
@@ -3,28 +3,32 @@ import { Minus } from 'lucide-react';
 
 const Hero = () => {
 	return (
-		<div className=' h-full w-full bg-green-300'>
-			<div className='flex  flex-col items-center justify-start gap-12 bg-blue-300 text-center text-2xl'>
-				<div className='flex justify-start text-start'>
+		<div className=' h-full w-full'>
+			<div className='flex flex-col items-center justify-start gap-24 text-center'>
+				<div className='flex w-full justify-start px-[25%] text-start font-caveat text-4xl'>
 					<h3>
-						<span>Accompagner</span> votre cheminement
+						<span className='text-primaryOne'>Accompagner</span> votre
+						cheminement
 					</h3>
 				</div>
-				<div className='flex flex-col justify-center'>
-					<h1>Métaphore Coaching</h1>
-					<div className='flex gap-4'>
+				<div className='flex flex-col items-center justify-center gap-3'>
+					<h1 className='text-6xl'>Métaphore Coaching</h1>
+					<div className='flex items-center gap-4 text-4xl'>
 						<h2>Formation</h2>
-						<Minus className='rotate-90' />
+						<div className='h-6 w-[3px] bg-black'></div>
 						<h2>Coaching</h2>
 					</div>
 				</div>
-				<div>
+				<div className='flex w-full justify-end px-[25%] font-caveat text-4xl'>
 					<h3>
-						<span>Développer</span> votre potentiel
+						<span className='highlighted rounded-xl text-secondaryOne'>
+							Développer
+						</span>{' '}
+						votre potentiel
 					</h3>
 				</div>
 			</div>
-			<div className='chess-bg h-24 w-48 -translate-x-12 rotate-90 rounded-t-full '></div>
+			<div className='chess-bg h-48 w-96 -translate-x-12 rotate-90 rounded-t-full'></div>
 		</div>
 	);
 };
