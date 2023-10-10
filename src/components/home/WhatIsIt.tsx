c0f517e5a891320f9bbfcd7997a344b8309ff59c
Clement SERIZAY <clement.serizay@gmail.com>
2023-10-10 22:31:44 +0200

    style: designing cards for section

--- src/components/home/WhatIsIt.tsx
+++ src/components/home/WhatIsIt.tsx
@@ -1,24 +1,52 @@
+import Image from 'next/image';
 import React from 'react';
+import { Button } from '../ui/button';
 
 const WhatIsIt = () => {
 	return (
-		<div className='m-auto flex h-screen w-[90%] items-center justify-center bg-blue-400'>
-			<div className='h-[500px] w-[40%] bg-red-300 p-12'>
-				<h2>Changer les possibles</h2>
-				<div>
-					<h3>Métaphore coaching, pour quoi ?</h3>
-					<div>
-						<p>
-							Nous créons ensemble un chemon personnalisé pour atteindre
-							votre objectif final en prenant en compte votre univers
-							personnel et vos images mentales.
-						</p>
-						<p>
-							Le coaching n'a pas pour but de vous transformer mais de
-							vous aider à trouver des solutions adaptées à qui vous êtes
-							et mieux vivre.
-						</p>
+		<div className='m-auto my-[10vh] flex h-[600px] w-[86%] justify-center gap-12'>
+			<div className='flex flex-1 flex-col items-center justify-center gap-6 3xl:justify-start'>
+				<Image
+					src='/chess-miror.png'
+					alt='Chess'
+					width={642}
+					height={446}
+					className='w-[25vw] '
+				/>
+				<p className='translate-x-[-50%] font-caveat text-4xl text-secondaryOne'>
+					Celui qui sait demander est{' '}
+					<span className='bg-secondaryOne px-2 text-whiteOne'>
+						{' '}
+						libre
+					</span>
+				</p>
+			</div>
+			<div className='flex h-[600px] w-[40%] flex-col'>
+				<div className='m-auto flex w-[500px] flex-col gap-10 bg-primaryOne/40 p-10'>
+					<div className='flex flex-col gap-3'>
+						<h2 className='font-caveat text-5xl font-bold'>
+							Changer les possibles
+						</h2>
+						<div className='h-[1px] w-full bg-secondaryOne/50'></div>
 					</div>
+					<div className='flex flex-col gap-4'>
+						<h3 className='font-inter text-xl'>
+							Métaphore coaching, pour quoi ?
+						</h3>
+						<div>
+							<p>
+								Nous créons ensemble un chemin personnalisé pour
+								atteindre votre objectif final en prenant en compte
+								votre univers personnel et vos images mentales.
+							</p>
+							<p>
+								Le coaching n'a pas pour but de vous transformer mais de
+								vous aider à trouver des solutions adaptées à qui vous
+								êtes et mieux vivre.
+							</p>
+						</div>
+					</div>
+					<Button size={'md'}>En savoir plus</Button>
 				</div>
 			</div>
 		</div>
--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -10,7 +10,7 @@ const buttonVariants = cva(
 		variants: {
 			variant: {
 				default:
-					'rounded-xl bg-primary text-primary-foreground hover:bg-primary/90',
+					'rounded-xl bg-primary text-primary-foreground hover:bg-primary/70',
 				destructive:
 					'bg-destructive text-destructive-foreground hover:bg-destructive/90',
 				outline:
@@ -23,7 +23,8 @@ const buttonVariants = cva(
 			size: {
 				default: 'h-10 px-4 py-2',
 				sm: 'h-9 rounded-md px-3',
-				lg: 'h-11 rounded-md px-8',
+				md: 'h-10 rounded-md px-6 text-lg',
+				lg: 'h-11 rounded-md px-8 text-xl',
 				icon: 'h-10 w-10',
 			},
 		},
