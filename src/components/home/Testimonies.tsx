8d8358be82af32a5b6b24836fc250d6e37a48ad4
monkeycs60 <clement.serizay@gmail.com>
2023-10-27 15:47:29 +0200

    style: responsive testimonies

--- src/components/home/Testimonies.tsx
+++ src/components/home/Testimonies.tsx
@@ -23,15 +23,21 @@ const Testimonies = () => {
 		}
 	};
 	return (
-		<div className='containerBordureBriseeTwo relative flex h-[70vh] w-full flex-col items-center gap-6 pt-12 2xl:h-[50vh] 3xl:h-[36vh] '>
+		<div className='containerBordureBriseeTwo relative flex w-full flex-col items-center gap-6 pb-8 pt-12 lg:h-[70vh] 2xl:h-[50vh] 3xl:h-[36vh] '>
 			<div>
 				<h2 className='text-center text-2xl font-bold'>Témoignages</h2>
 			</div>
 			<div className='flex flex-col items-center gap-4'>
 				<div className='flex w-full px-[130px] 2xl:px-[300px] 3xl:px-[400px] '>
-					<Image src='/quote.svg' alt='quote' width={80} height={42} />
+					<Image
+						src='/quote.svg'
+						alt='quote'
+						width={80}
+						height={42}
+						className='h-[30px] w-[60px] lg:h-[42px] lg:w-[80px] '
+					/>
 				</div>
-				<div className=' flex h-[170px] w-[60%] translate-x-[-50px] flex-col gap-6 2xl:w-[45%]'>
+				<div className=' flex w-[80%] flex-col gap-6 lg:h-[170px] lg:w-[60%] lg:translate-x-[-50px] 2xl:w-[45%]'>
 					<p className='first-letter:indent-6'>
 						{testimoniesList[testimonyIndex].text}
 					</p>
@@ -41,7 +47,7 @@ const Testimonies = () => {
 						{testimoniesList[testimonyIndex].position}
 					</p>
 				</div>
-				<div className='mt-4 flex w-[20%] justify-center gap-14 2xl:mt-8'>
+				<div className='mt-4 flex justify-center gap-14 lg:w-[20%] 2xl:mt-8'>
 					<MoveLeft
 						className='h-[45px] w-[45px] cursor-pointer select-none hover:text-primaryOne'
 						onClick={previousImage}
