8100efe869051a1db140d3705a07ce156406a023
Clement SERIZAY <clement.serizay@gmail.com>
2024-01-07 01:39:04 +0100

    fix: cloudinary og

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,11 +2,20 @@ import NavBar from '@/components/layout/NavBar';
 import './globals.css';
 import type { Metadata } from 'next';
 import Footer from '@/components/layout/Footer';
+import { getCldOgImageUrl } from 'next-cloudinary';
 
 export const metadata: Metadata = {
 	title: 'Métaphore coaching',
 	description: 'Christophe Jacques, votre coach professionnel',
-	// openGraph: { title: "Métaphore coaching", description: "Christophe Jacques, votre coach professionnel" ,images: [{ url: '/final-logo.svg' }] },
+	openGraph: {
+		title: 'Métaphore coaching',
+		description: 'Christophe Jacques, votre coach professionnel',
+		images: [
+			getCldOgImageUrl({
+				src: 'k6eowvgwc1w5xsg0q3xp',
+			}),
+		],
+	},
 };
 
 export default function RootLayout({
